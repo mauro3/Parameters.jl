a1ed60d5a4f21898197a3a70800964c4af09a7b8
Zentrik <Zentrik@users.noreply.github.com>
2023-06-20 10:57:25 +0100

    Remove julia version check

--- test/runtests.jl
+++ test/runtests.jl
@@ -380,33 +380,31 @@ end
 
 ### Smart Assertions
 
-@static if VERSION >= v"1.5" # Need static otherwise it tries to evaluate code below which fails
-    @with_kw struct MT12Smart
-        a=5; @smart_assert a>=5
-        b
-        @smart_assert b>a
-    end
-
-    @test_throws AssertionError MT12Smart(b=2)
-    @test_throws AssertionError MT12Smart(a=1,b=2)
-    @test MT12Smart(b=6)==MT12Smart(5,6)
-
-    # only asserts allowed if no inner constructors
-    @test_throws ErrorException Parameters.with_kw(:(struct MT13Smart
-                                                    a=5;
-                                                    @smart_assert a>=5
-                                                    MT13Smart(a) = new(8)
-                                                    end),
-                                                @__MODULE__)
-
-    # issue #29: assertions with parameterized types
-    @with_kw struct MT12aSmart{R}
-        a::Array{R,1}
-        @smart_assert 1 == length(a)
-    end
-    @test_throws AssertionError MT12aSmart([1,2])
-    @test MT12aSmart([1]).a==MT12aSmart(a=[1]).a
+@with_kw struct MT12Smart
+    a=5; @smart_assert a>=5
+    b
+    @smart_assert b>a
 end
+
+@test_throws AssertionError MT12Smart(b=2)
+@test_throws AssertionError MT12Smart(a=1,b=2)
+@test MT12Smart(b=6)==MT12Smart(5,6)
+
+# only asserts allowed if no inner constructors
+@test_throws ErrorException Parameters.with_kw(:(struct MT13Smart
+                                                a=5;
+                                                @smart_assert a>=5
+                                                MT13Smart(a) = new(8)
+                                                end),
+                                            @__MODULE__)
+
+# issue #29: assertions with parameterized types
+@with_kw struct MT12aSmart{R}
+    a::Array{R,1}
+    @smart_assert 1 == length(a)
+end
+@test_throws AssertionError MT12aSmart([1,2])
+@test MT12aSmart([1]).a==MT12aSmart(a=[1]).a
 
 ####
 # issue 10: infer type parameters from kw-args
